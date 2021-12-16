1bbe6e2013b547f93848122cbc774509e9a74046
Lam <LamDuong.inbox@gmail.com>
2021-12-15 20:29:30 -0800

    commit tree 24b65533c0e2d7798d13a713da646c8b94fd4319

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,48 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/toys')
+      .then(res => res.json())
+      .then(setToys);
+  }, [])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleAddToy(newToy) {
+    const updatedToys = [newToy, ...toys];
+    setToys(updatedToys);
+  }
+
+  function handleDeleteToy(targetToy) {
+    const updatedToys = toys.filter(toy => toy.id !== targetToy.id);
+    setToys(updatedToys);
+  }
+
+  function handleUpdateToy(updatedToy) {
+    const updatedToys = toys.map(toy => 
+      toy.id === updatedToy.id ? updatedToy : toy
+    )
+
+    setToys(updatedToys);
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAdd={handleAddToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer 
+        toys={toys}
+        onDelete={handleDeleteToy}
+        onUpdate={handleUpdateToy}
+      />
     </>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toys, onDelete, onUpdate }) {
+  const toyCards = toys.map(toy => (
+    <ToyCard 
+      key={toy.id}
+      toy={toy}
+      onDelete={onDelete}
+      onUpdate={onUpdate}
+    />
+  ))
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toyCards}</div>
   );
 }
 
